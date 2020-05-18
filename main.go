8339a7203ffb9ff8085001d1220293ba90cec086
James Benze <benzejaa@gmail.com>
2020-05-18 06:53:49 -0400

    Tag version v0.1

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "toolbox",
-	Short: "Toolbox is a tool vendoring helper",
-	Long:  "Toolbox sits on top of go's powerful module engine, and leverages it to vendor your executables",
+	Use:     "toolbox",
+	Short:   "Toolbox is a tool vendoring helper",
+	Long:    "Toolbox sits on top of go's powerful module engine, and leverages it to vendor your executables",
+	Version: "v0.1",
 }
 
 func main() {
