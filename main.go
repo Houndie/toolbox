1735a272d7eb99fd6ca553e08f698942c3f137be
James Benze <benzejaa@gmail.com>
2020-05-20 06:46:36 -0400

    Set version 0.2

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var rootCmd = &cobra.Command{
 	Use:     "toolbox",
 	Short:   "Toolbox is a tool vendoring helper",
 	Long:    "Toolbox sits on top of go's powerful module engine, and leverages it to vendor your executables",
-	Version: "v0.1",
+	Version: "v0.2",
 }
 
 func main() {
